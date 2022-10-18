5f724b7f12b276f9e83728a7cfde6f56220f4492
Dannyfsp <femoscopilala@gmail.com>
2022-10-17 18:31:31 -0700

    Rectangle class that defines a rectangle based on 3-rectangle.py

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""
+Rectangle module that contains a class that defines a rectangle
+Also returns the area and perimeter of the rectangle
+Also uses the __str__ and __repl__ method
+"""
+
+
+class Rectangle:
+    """this class represents a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initializing this rectangle class
+        Args:
+            width(int): represents the width of the rectangle
+            height(int): represents the height of the rectangle
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get or set the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is int:
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        """Get or set the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is int:
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return self.__width * 2 + self.__height * 2
+
+    def __str__(self):
+        """presents a diagram of the rectangle object"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rectangle = ""
+        for column in range(self.__height):
+            for row in range(self.__width):
+                rectangle += "#"
+            if column < self.__height - 1:
+                rectangle += "\n"
+        return rectangle
+
+    def __repl__(self):
+        """Sets the repr behaviour of the Rectangle object"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
