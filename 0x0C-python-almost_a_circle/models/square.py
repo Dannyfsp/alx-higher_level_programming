ae2c1803137338c8ff7d7c84d234e45ffef49091
Dannyfsp <femoscopilala@gmail.com>
2022-10-30 06:39:43 -0700

    a class Square that inherits from Rectangle and adding public getter and setter size

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class than inherits from Rectangle class
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents a Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize a new Square
+        Args:
+            size(int): The size of the new square
+            x(int): The x coordinate of the new square
+            y(int): The y coordinate of the new square
+            id(int): The identity of the new square
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get/set the size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Return the print() and str() representation of a square"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,6 +5,8 @@ This module defines unittests for base.py
 import os
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase_instantiation(unittest.TestCase):
