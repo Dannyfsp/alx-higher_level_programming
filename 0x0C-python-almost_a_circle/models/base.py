84ccfdf2c31f499df633c7aeb9dc01778ef08088
Dannyfsp <femoscopilala@gmail.com>
2022-10-30 19:48:17 -0700

    adding class method that returns a list of instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -80,3 +80,17 @@ class Base:
                 new = cls(1)
             new.update(**dictionary)
             return new
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Returns a list of classes instantiated from a file of JSON strings
+        Reads from `<cls.__name__>.json`
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
