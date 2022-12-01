595bdc52b4ce3be0db9d0279ec59bd91d63941c4
Dannyfsp <femoscopilala@gmail.com>
2022-11-30 21:05:45 -0800

    Hot fix for A script that lists all shows without the genre Comdey in hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -7,7 +7,7 @@
 SELECT tv_shows.title
 FROM tv_shows
 WHERE title NOT IN (
-	SELECT tv_genres.title
+	SELECT tv_shows.title
 	FROM tv_shows
 	INNER JOIN tv_show_genres
 	ON tv_shows.id = tv_show_genres.show_id
@@ -15,4 +15,4 @@ WHERE title NOT IN (
 		ON tv_show_genres.genre_id = tv_genres.id
 	WHERE tv_genres.name = 'Comedy'
 )
-ORDER BY tv_genres.title;
+ORDER BY tv_shows.title;
