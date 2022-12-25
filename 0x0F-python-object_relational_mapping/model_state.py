775fc0e3edef1078c07bb80ad4362ddd3b282b98
Dannyfsp <femoscopilala@gmail.com>
2022-12-24 16:00:20 -0800

    A python file that contains the state class that inherits from Base class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script defines a State class that
+inherits from Base class to work with MySQLAlchemy ORM
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    This class links to the 'states' table of our database
+    Attributes:
+        __tablename__(str): The table name of the class
+        id(int): The State id of the class
+        name(Str): The State name of the class
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
