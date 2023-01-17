67fdf874aa09e2b885f66c93d71646fedbce53a1
Dannyfsp <femoscopilala@gmail.com>
2023-01-17 15:55:10 -0800

    Sends a POST request to http://0.0.0.0:5000/search_user with a given letter

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,11 +8,7 @@ import requests
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        letter = ""
-    else:
-        sys.argv[1]
-
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
