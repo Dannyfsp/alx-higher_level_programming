7caa341402f7fe130e66b6b8c44334b3c93ece1f
Dannyfsp <femoscopilala@gmail.com>
2023-01-17 15:23:12 -0800

    Takes in a URL, sends a request to the URL and displays the body using requests package

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,10 +2,11 @@
 """
 Takes in a URL, sends a request to the URL and displays the body
 """
+import sys
+import requests
+
 
 if __name__ == '__main__':
-    import requests
-    import sys
     r = requests.get(sys.argv[1])
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
