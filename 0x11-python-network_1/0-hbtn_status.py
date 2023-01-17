c794bf8d354405e450de849d9d5941c27a9962ee
Dannyfsp <femoscopilala@gmail.com>
2023-01-17 14:36:54 -0800

    fix - A Python script that fetches a URL

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf* content: {}".format(body.decode("utf-8")))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
