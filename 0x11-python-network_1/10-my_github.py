e6a254cb6ff9e6fd41d19eb451f6a0bf9e5fdff1
Dannyfsp <femoscopilala@gmail.com>
2023-01-17 15:47:20 -0800

    takes your GitHub credentials (username and password) and uses the GitHub API to display your id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Takes my Github credentials (username and password) and uses the 
+Github API to display my id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    try:
+        r = requests.get('https://api.github.com/user', auth=(
+            username, password)).json()
+        print(r.get('id'))
+    except:
+        print("None")
