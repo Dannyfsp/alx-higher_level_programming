b58a33bc9c9908bd7d99bbe4672274aa55e98173
Dannyfsp <femoscopilala@gmail.com>
2023-02-09 22:03:50 +0000

    A JS script to read from a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
-fs.readFile(process.argv[2], "utf-8", (err, data) => {
-  if (err) {
-    console.log(err);
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
   } else {
     console.log(data);
   }
